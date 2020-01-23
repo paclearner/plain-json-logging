825549f6fa434bfc3d6bc51380ada6d7300343d8
paclearner <pac.learner@gmail.com>
2020-01-24 06:07:12 +0900

    support python2.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='plain-json-logging',
-    version='0.1.0',
+    version='0.2.0',
     description='A simple logging in JSON',
     long_description=codecs.open('README.rst', 'r', 'utf-8').read(),
     author='Junnosuke Moriya',
@@ -14,13 +14,19 @@ setup(
     url='https://github.com/paclearner/plain-json-logging',
     license='Apache License 2.0',
     packages=['plain_json_logging'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries',
     ],
     test_suite='tests',
